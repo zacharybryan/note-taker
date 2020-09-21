8453eee9b64aba35dee9a1206708ac5955aee1e6
Zach Bryan <zacharybryan@icloud.com>
2020-09-21 02:29:47 -0700

    created note array

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ let PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'db')))
 
+const note = [];
+
+
+
 // Create Routes
 
 // GET '*' index.html]
