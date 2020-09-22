20a46b6218ad2bb2bd577c02b1077f552cf936ed
Zach Bryan <zacharybryan@icloud.com>
2020-09-21 22:24:34 -0700

    saves notes to sidebar

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,15 +12,27 @@ const path = require("path");
 module.exports = function (app) {
 
     app.get("/api/notes", function (req, res) {
-        notesData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), {encoding: "utf8"}))
+        notesData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), { encoding: "utf8" }))
         res.json(notesData);
         console.log("this is notes data", noteData);
     });
 
     app.post("/api/notes", function (req, res) {
-            let newNote = req.body;
-            notesData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), {encoding: "utf8"}));
-            console.log("new note", newNote);
+        let newNote = req.body;
+        notesData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), { encoding: "utf8" }));
+
+        newNote.routeName = newNote.title.replace(/ /g, "").toLowerCase();
+        console.log("new note", newNote);
+
+        notesData.push(newNote);
+
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesData, null, 4), function(err) {
+            if (err) console.log(err);
+
+            console.log("note added");
+        })
+
+        res.json(newNote);
     });
 
     app.post("/api/clear", function (req, res) {
