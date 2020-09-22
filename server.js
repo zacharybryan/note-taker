389bce735301a6f60afb16f0ff4a8491e7c4c179
Zach Bryan <zacharybryan@icloud.com>
2020-09-21 21:45:39 -0700

    added static for db

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static('db'));
 
 require("./routes/htmlRoutes")(app);
 
